2160a971dbc56778157e549a7792bb37e7346650
Afiatinaliaa <afiatinaliaazani@gmail.com>
2023-11-22 09:57:24 +0700

    feat/tugas-12-api

--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 //import 'package:sanberappflutter/Tugas/Tugas-11/HomeScreen.dart';
 //import 'package:sanberappflutter/Tugas/Tugas-11/LoginScreen.dart';
-import 'package:sanberappflutter/Tugas/Tugas-11/NavBar.dart';
+// import 'package:sanberappflutter/Tugas/Tugas-11/NavBar.dart';
+import 'package:sanberappflutter/Tugas/Tugas-12/get_data_detail_screen.dart';
+import 'package:sanberappflutter/Tugas/Tugas-12/get_data_screen.dart';
 
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  //const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -23,7 +26,8 @@ class MyApp extends StatelessWidget {
       //home: Telegram(),
       //home: HomeScreen(),
       //home: LoginScreen(),
-      home: NavigationBarScreen(),
+      //home: NavigationBarScreen(),
+      home : GetDataDetailScreen(),
     );
   }
 }
